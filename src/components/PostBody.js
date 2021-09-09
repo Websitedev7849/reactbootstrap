415212ef006e3e33b94a020e504646e6eae8580e
Siddharth Pawar <websitedev7849@gmail.com>
2021-09-09 21:34:12 +0530

    removed wrapping around card from PostBody.js

--- src/components/PostBody.js
+++ src/components/PostBody.js
@@ -9,38 +9,34 @@ const PostBody = (prop) => {
   console.log(post);
  
   return (
-    <section className="d-flex flex-col py-3 justify-content-center align-items-center">
         
-        <div className="card">
-            <img src={post} className="card-img-top" alt="post 1" />
-            <div className="card-body position-relative">
-                <h5 className="card-title py-2 small">{cardTitle}</h5>
-                <p className="card-text bold">
-                   <strong className="text-break"> {article} </strong>
-                   <br />
-                   <span className="text-muted">{textMuted}</span>
-                </p>
+    <div className="card my-4 d-inline-flex">
+        <img src={post} className="card-img-top" alt="post 1" />
+        <div className="card-body position-relative">
+            <h5 className="card-title py-2 small">{cardTitle}</h5>
+            <p className="card-text bold">
+                <strong className="text-break"> {article} </strong>
+                <br />
+                <span className="text-muted">{textMuted}</span>
+            </p>
 
-                <img className="position-absolute py-2 moreOptions" src={moreOption} alt="more options"/>
+            <img className="position-absolute py-2 moreOptions" src={moreOption} alt="more options"/>
 
-                <div className="bottom">
-                  <img src={personPhoto} alt="person 1" />
-                  <p className="lead fs-6 px-2 py-sm-2 name"> {name} </p>
-                  <p className="text-muted px-2 py-sm-2 views">
-                    <img src={viewsSVG} className="d-none d-sm-inline" alt="views"/>
-                    <span className="px-sm-1">{views}</span>
-                  </p>
-                  <button type="button" style={{ background: "#F1F3F5" }} className="btn btn-light shareBtn">
-                    <img src={shareSVG} alt="share"  />
-                    <span className="fs-6 px-1 px-sm-2 fw-lighter d-sm-none">Share</span>
-                  </button>
-                </div>
-
+            <div className="bottom">
+              <img src={personPhoto} alt="person 1" />
+              <p className="lead fs-6 px-2 py-sm-2 name"> {name} </p>
+              <p className="text-muted px-2 py-sm-2 views">
+                <img src={viewsSVG} className="d-none d-sm-inline" alt="views"/>
+                <span className="px-sm-1">{views}</span>
+              </p>
+              <button type="button" style={{ background: "#F1F3F5" }} className="btn btn-light shareBtn">
+                <img src={shareSVG} alt="share"  />
+                <span className="fs-6 px-1 px-sm-2 fw-lighter d-sm-none">Share</span>
+              </button>
             </div>
+
         </div>
-
-
-    </section>
+    </div>
   )
 }
 
